daaaa5528dd72ca046ee8f3667efaeb9d5dcb946
Hardware-Man <kmishra686@gmail.com>
2020-11-16 23:54:01 -0500

    JUnit test and some other small changes

--- src/functions/OrderTest.java
+++ src/functions/OrderTest.java
@@ -37,8 +37,8 @@ class OrderTest {
         testSandwich = new Fish();
         test.add(new OrderLine(0,testSandwich,testSandwich.price()));
         assertEquals(2,test.getOrderLines().size());
-        test.remove(new OrderLine(2,testSandwich,testSandwich.price()));
-        assertEquals((new OrderLine(1,new Chicken(),new Chicken().price())).toString(),test.getOrderLines().get(0).toString());
+        test.remove(new OrderLine(1,new Chicken(),new Chicken().price()));
+        assertEquals((new OrderLine(1,new Fish(),new Fish().price())).toString(),test.getOrderLines().get(1).toString());
     }
 
     @Test
